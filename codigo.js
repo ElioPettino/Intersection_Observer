53c0b553beeba539987d67dea84db6e57144babd
Elio Pettino <eliopettino@gmail.com>
2024-07-27 20:52:52 -0300

    Primer commit

---
+++ codigo.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const cajas = document.querySelectorAll(".caja");
+
+const verifyVisibility = (entries) =>
+{
+	for (const entry of entries)
+	{
+		if(entry.isIntersecting) console.log("se esta viendo la caja: ",entry.target.textContent);
+	}
+}
+
+
+
+const observer = new IntersectionObserver(verifyVisibility);
+
+for (const caja of cajas)
+{
+	observer.observe(caja);
+}
+
